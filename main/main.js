e6713d0471bacb7d544c3f3155194b00e9dae731
mahuanfy <mahuanfy@foxmail.com>
2019-04-10 10:44:26 +0800

    Init project

--- main/main.js
+++ main/main.js
@@ -1,14 +1,3 @@
 module.exports = function main(inputs) {
-    let price = 0;
 
-    if (inputs.distance <= 2) {
-        price = 6;
-    } else if (2 < inputs.distance && inputs.distance <= 8) {
-        price = 6 + ((inputs.distance - 2) * 0.8);
-    } else {
-        price = 10.8 + ((inputs.distance - 8) * 1.2);
-    }
-    price += (inputs.parkTime * 0.25);
-
-    return Math.round(price);
 };
